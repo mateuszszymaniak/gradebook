0749f55000bd1f5629da1a181133fbd81fc8543f
Mateusz Szymaniak <maszipl@gmail.com>
2022-10-10 21:39:04 +0200

    Added block parent window after pressed register button

--- src/main/java/com/example/gradebook/HelloApplication.java
+++ src/main/java/com/example/gradebook/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.gradebook;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
