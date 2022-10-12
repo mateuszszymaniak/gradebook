1bbb68f8b5ec1f260905236391d9704ec0024880
Mateusz Szewczyk <m.szewczyk58@gmail.com>
2022-10-12 10:54:43 +0200

    DB browser plugin added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.example.gradebook {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires java.sql;
 
     opens com.example.gradebook to javafx.fxml;
     exports com.example.gradebook;
