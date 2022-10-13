b478d85d2bf2a6cd95a3cc03f3b188c77f0b5750
Mateusz Szewczyk <m.szewczyk58@gmail.com>
2022-10-13 13:07:30 +0200

    Grade class's field updated

--- src/main/java/com/example/gradebook/Grade.java
+++ src/main/java/com/example/gradebook/Grade.java
@@ -4,17 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class Grade {
-    @Getter @Setter private int id, studentId, UserId;
+    @Getter @Setter private int id, studentId, userId;
     @Getter @Setter private double grade;
     @Getter @Setter private String subject, type, comment;
 
-    public Grade(int id, double grade, String subject, String type, String comment, int studentId, int UserId) {
+    public Grade(int id, double grade, String subject, String type, String comment, int studentId, int userId) {
         this.id = id;
         this.grade = grade;
         this.subject = subject;
         this.type = type;
         this.comment = comment;
         this.studentId = studentId;
-        this.UserId = UserId;
+        this.userId = userId;
     }
 }
