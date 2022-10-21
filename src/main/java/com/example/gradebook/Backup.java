ea7f7d2d773d285a1435c1087e30069199df98dc
Mateusz Szewczyk <m.szewczyk58@gmail.com>
2022-10-21 17:56:46 +0200

    Added import backup method

--- src/main/java/com/example/gradebook/Backup.java
+++ src/main/java/com/example/gradebook/Backup.java
@@ -1,17 +1,14 @@
 package com.example.gradebook;
 
 import javafx.util.Pair;
-import org.jdom2.Attribute;
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.Parent;
+import org.jdom2.*;
+import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.io.*;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class Backup extends DBTransaction{
@@ -24,6 +21,12 @@ public class Backup extends DBTransaction{
         this.students = getStudents_mechanism("SELECT * FROM students");
         this.grades = getGrades_mechanism("SELECT * FROM grades");
     }
+
+
+
+    // ----------------------
+    // Export backup
+    // ----------------------
 
     public void createBackup() {
         Document doc = xmlBegin();
@@ -138,5 +141,103 @@ public class Backup extends DBTransaction{
         catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+
+    // ----------------------
+    // Import backup
+    // ----------------------
+
+    public boolean importBackup() {
+        if (initImport()) {
+            Element xml = xmlReadBegin();
+            if (xml != null) {
+                Element gradebook = xml.getChild("gradebook");
+                if (!processUser(gradebook.getChildren("users"))) return false;
+                if (!processStudent(gradebook.getChildren("students"))) return false;
+                if (!processGrade(gradebook.getChildren("grades"))) return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean initImport() {
+        try {
+            for (int i=(tables.length-1); i>=0;i--) {
+                String init = "DELETE from " + tables[i];
+                statement.execute(init);
+                init = "ALTER TABLE " + tables[i] + " AUTO_INCREMENT=1";
+                statement.execute(init);
+            }
+        } catch (SQLException e) {
+            System.err.println("Import init error");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private Element xmlReadBegin() {
+        File input = new File("backup.xml");
+        SAXBuilder saxBuilder = new SAXBuilder();
+        Document doc = null;
+        try {
+            doc = saxBuilder.build(input);
+        } catch (JDOMException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return doc.getRootElement();
+    }
+
+    private boolean processUser(List<Element> usersList) {
+        String login, password;
+        List<Element> users = usersList.get(0).getChildren("user");
+        for (int i=0; i<users.size();i++) {
+            Element user = users.get(i);
+            login = user.getChildText("login");
+            password = user.getChildText("password");
+            if(!registerUser(login, password)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean processStudent(List<Element> studentsList) {
+        String surname, name, studentGroup, schoolYear;
+        List<Element> students = studentsList.get(0).getChildren("student");
+        for (int i=0;i<students.size();i++) {
+            Element student = students.get(i);
+            surname = student.getChildText("surname");
+            name = student.getChildText("name");
+            studentGroup = student.getChildText("studentGroup");
+            schoolYear = student.getChildText("schoolYear");
+            if(!addStudent(surname, name, studentGroup, schoolYear)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean processGrade(List<Element> gradesList) {
+        double grade;
+        String subject, type, comment;
+        int studentId, userId;
+        List<Element> grades = gradesList.get(0).getChildren("grade");
+        for (int i=0;i<grades.size();i++) {
+            Element gr = grades.get(i);
+            grade = Double.parseDouble(gr.getChildText("grade"));
+            subject = gr.getChildText("subject");
+            type = gr.getChildText("type");
+            comment = gr.getChildText("comment");
+            studentId = Integer.parseInt(gr.getChildText("studentId"));
+            userId = Integer.parseInt(gr.getChildText("userId"));
+            if(!addGrade(grade, subject, type, comment, studentId, userId)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
