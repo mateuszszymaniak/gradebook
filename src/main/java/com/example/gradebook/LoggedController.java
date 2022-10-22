6a5f514263a6261934adfa39f8e3b80cd7e9f31b
Mateusz Szymaniak <maszipl@gmail.com>
2022-10-22 22:43:18 +0200

    Trying to create working checkbox
    Checxbox is displaying but doesn't work :(

--- src/main/java/com/example/gradebook/LoggedController.java
+++ src/main/java/com/example/gradebook/LoggedController.java
@@ -1,5 +1,7 @@
 package com.example.gradebook;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,8 +10,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -21,15 +26,17 @@ import java.util.List;
 public class LoggedController {
     @FXML private Button addStudent;
     @FXML private Button addGrade;
+    @FXML private Button deleteStudent;
     @FXML private TableView<Student> table;
     @FXML private TableColumn<Student, String> nazwisko;
     @FXML private TableColumn<Student, String> imie;
     @FXML private TableColumn<Student, String> klasa;
     @FXML private TableColumn<Student, String> rokSzkolny;
+    @FXML private TableColumn<Student, Boolean> toDelete;
 
-    @FXML private void initialize(){
+    @FXML private void initialize() {
         DBTransaction db = new DBTransaction();
-        if(db.ifStudentExist() == false){
+        if (db.ifStudentExist() == false) {
             addGrade.setVisible(false);
             table.setVisible(false);
         }
@@ -39,9 +46,15 @@ public class LoggedController {
             imie.setCellValueFactory(c -> new SimpleStringProperty(data.getName()));
             klasa.setCellValueFactory(c -> new SimpleStringProperty(data.getStudentGroup()));
             rokSzkolny.setCellValueFactory(c -> new SimpleStringProperty(data.getSchoolYear()));
+            //below line needs to be deleted when changing value of checkbox and deleting will works
+            toDelete.setVisible(true);
+            toDelete.setCellFactory(c -> new CheckBoxTableCell<>());
+            //TODO add listener to changed value of checkbox
+            //TODO deleting by selected rows maybe helps (https://www.youtube.com/watch?v=NFiMj6jszGM&ab_channel=java-techtutorials)
             table.getItems().add(data);
         }
     }
+
     public void onClickBtnAddStudent(ActionEvent actionEvent) throws IOException {
         Parent fxmlLoader = FXMLLoader.load(HelloController.class.getResource("addStudent-view.fxml"));
         Stage stage = new Stage();
@@ -58,5 +71,9 @@ public class LoggedController {
         stage.initModality(Modality.WINDOW_MODAL);
         stage.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
         stage.show();
+    }
+
+    public void onClickBtnDeleteStudent(ActionEvent actionEvent) {
+        toDelete.setVisible(true);
     }
 }
--- src/main/java/com/example/gradebook/Student.java
+++ src/main/java/com/example/gradebook/Student.java
@@ -1,5 +1,7 @@
 package com.example.gradebook;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.scene.control.CheckBox;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,11 +9,21 @@ public class Student {
     @Getter @Setter private int id;
     @Getter @Setter private String surname, name, studentGroup, schoolYear;
 
+    @Getter @Setter private Boolean toDelete = false;
+
     public Student(int id, String surname, String name, String studentGroup, String schoolYear) {
         this.id = id;
         this.surname = surname;
         this.name = name;
         this.studentGroup = studentGroup;
         this.schoolYear = schoolYear;
+    }
+    public Student(int id, String surname, String name, String studentGroup, String schoolYear, Boolean toDelete) {
+        this.id = id;
+        this.surname = surname;
+        this.name = name;
+        this.studentGroup = studentGroup;
+        this.schoolYear = schoolYear;
+        this.toDelete = toDelete;
     }
 }
