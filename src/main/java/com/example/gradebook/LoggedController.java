14aabed33814383b9a32599ce5b82985f2574de1
Mateusz Szymaniak <maszipl@gmail.com>
2022-10-24 09:45:37 +0200

    Trying to send variable between windows

--- src/main/java/com/example/gradebook/LoggedController.java
+++ src/main/java/com/example/gradebook/LoggedController.java
@@ -9,13 +9,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -25,20 +24,36 @@ import java.util.List;
 
 public class LoggedController {
     @FXML private Button addStudent;
-    @FXML private Button addGrade;
+    @FXML private Button editStudent;
     @FXML private Button deleteStudent;
+    @FXML private Button addGrade;
+    @FXML private Button editGrade;
+    @FXML private Button deleteGrade;
     @FXML private TableView<Student> table;
     @FXML private TableColumn<Student, String> nazwisko;
     @FXML private TableColumn<Student, String> imie;
     @FXML private TableColumn<Student, String> klasa;
     @FXML private TableColumn<Student, String> rokSzkolny;
     @FXML private TableColumn<Student, Boolean> toDelete;
+    @FXML private Tab oceny;
+    @FXML private Pane contentPane;
 
     @FXML private void initialize() {
         DBTransaction db = new DBTransaction();
+        //make fun with copilot send login between stages :)
+        //String login = contentPane.getChildren().toString();
+        //https://dev.to/devtony101/javafx-3-ways-of-passing-information-between-scenes-1bm8
+        //https://stackoverflow.com/questions/14187963/passing-parameters-javafx-fxml
         if (db.ifStudentExist() == false) {
+            editStudent.setVisible(false);
+            deleteStudent.setVisible(false);
             addGrade.setVisible(false);
+            editGrade.setVisible(false);
+            deleteGrade.setVisible(false);
             table.setVisible(false);
+        } else if (db.ifGradeExist() == false){
+            editGrade.setVisible(false);
+            deleteGrade.setVisible(false);
         }
         List<Student> students = db.getStudents_byId(0);
         for (Student data : students) {
@@ -47,7 +62,7 @@ public class LoggedController {
             klasa.setCellValueFactory(c -> new SimpleStringProperty(data.getStudentGroup()));
             rokSzkolny.setCellValueFactory(c -> new SimpleStringProperty(data.getSchoolYear()));
             //below line needs to be deleted when changing value of checkbox and deleting will works
-            toDelete.setVisible(true);
+            //toDelete.setVisible(true);
             toDelete.setCellFactory(c -> new CheckBoxTableCell<>());
             //TODO add listener to changed value of checkbox
             //TODO deleting by selected rows maybe helps (https://www.youtube.com/watch?v=NFiMj6jszGM&ab_channel=java-techtutorials)
@@ -55,8 +70,11 @@ public class LoggedController {
         }
     }
 
+
     public void onClickBtnAddStudent(ActionEvent actionEvent) throws IOException {
         Parent fxmlLoader = FXMLLoader.load(HelloController.class.getResource("addStudent-view.fxml"));
+
+
         Stage stage = new Stage();
         stage.setScene(new Scene(fxmlLoader));
         stage.initModality(Modality.WINDOW_MODAL);
@@ -71,6 +89,7 @@ public class LoggedController {
         stage.initModality(Modality.WINDOW_MODAL);
         stage.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
         stage.show();
+
     }
 
     public void onClickBtnDeleteStudent(ActionEvent actionEvent) {
